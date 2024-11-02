fcb65791fda0307b4550d0ffa9c8ec8989f12f6f
Atul <100551438+Steve-atul@users.noreply.github.com>
2024-11-02 20:43:10 +0530

    Update newRequest.js

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://projectmanagement-backend-sqb4.onrender.com",
+  baseURL: "https://projectmanagement-backend-971d.onrender.com",
   // baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
 });
